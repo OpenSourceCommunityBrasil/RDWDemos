459524cfd7a90e80d665e2174d19c45fca5be2a1
Mobius One <mobius1qwe@gmail.com>
2022-11-30 14:07:10 -0300

    - Correção de demo lazarus/fullserver

--- Lazarus/FullServer/src/udatamodule.pas
+++ Lazarus/FullServer/src/udatamodule.pas
@@ -3,10 +3,14 @@ unit udatamodule;
 interface
 
 uses
-  SysUtils, Classes, IBConnection, sqldb, db, uRESTDWDatamodule, Dialogs,
-  ZConnection, ZDataset, uRESTDWDataUtils, uRESTDWConsts, uprincipal,
-  uRESTDWServerEvents, uRESTDWServerContext, uRESTDWDriverZEOS,
-  uRestDWLazDriver, uRESTDWParams, uRESTDWBasicDB;
+  SysUtils, Classes, IBConnection, sqldb, DB, Dialogs,
+
+  uRESTDWDatamodule, uRESTDWDataUtils, uRESTDWConsts, uRESTDWServerContext,
+  uRESTDWDriverZEOS, uRESTDWServerEvents, uRestDWLazDriver, uRESTDWParams,
+  uRESTDWBasicDB,
+
+  ZConnection, ZDataset,
+  uprincipal;
 
 type
 
@@ -24,13 +28,14 @@ type
     SQLTransaction1: TSQLTransaction;
     ZConnection1: TZConnection;
     ZQuery1: TZQuery;
-    procedure DataModuleGetToken(Welcomemsg, AccessTag: String;
+    procedure DataModuleGetToken(Welcomemsg, AccessTag: string;
       Params: TRESTDWParams; AuthOptions: TRESTDWAuthTokenParam;
-      var ErrorCode: Integer; var ErrorMessage: String; var TokenID: String;
-      var Accept: Boolean);
+      var ErrorCode: integer; var ErrorMessage: string; var TokenID: string;
+      var Accept: boolean);
     procedure IBConnection1BeforeConnect(Sender: TObject);
     procedure RESTDWServerEventsEventsservertimeReplyEvent(
-      var Params: TRESTDWParams; var Result: String);
+      var Params: TRESTDWParams;
+      var Result: string);
     procedure ServerMethodDataModuleCreate(Sender: TObject);
     procedure ZConnection1BeforeConnect(Sender: TObject);
   private
@@ -50,136 +55,136 @@ implementation
 
 procedure TServerMethodDM.ServerMethodDataModuleCreate(Sender: TObject);
 begin
- RESTDWPoolerZEOS.Active := RestDWForm.cbPoolerState.Checked;
+  RESTDWPoolerZEOS.Active := fPrincipal.cbPoolerState.Checked;
 end;
 
 procedure TServerMethodDM.ZConnection1BeforeConnect(Sender: TObject);
-VAR
-  Driver_BD: STRING;
-  Porta_BD: STRING;
-  Servidor_BD: STRING;
-  DataBase: STRING;
-  Pasta_BD: STRING;
-  Usuario_BD: STRING;
-  Senha_BD: STRING;
-BEGIN
-   database:= RestDWForm.EdBD.Text;
-   Driver_BD := RestDWForm.CbDriver.Text;
-   If RestDWForm.CkUsaURL.Checked Then
-    Servidor_BD := RestDWForm.EdURL.Text
-   Else
-    Servidor_BD := RestDWForm.DatabaseIP;
-   Case RestDWForm.CbDriver.ItemIndex Of
-    0 : Begin
-         Pasta_BD := IncludeTrailingPathDelimiter(RestDWForm.EdPasta.Text);
-         Database := RestDWForm.edBD.Text;
-         Database := Pasta_BD + Database;
-        End;
-    1 : Database := RestDWForm.EdBD.Text;
-   End;
-   Porta_BD   := RestDWForm.EdPortaBD.Text;
-   Usuario_BD := RestDWForm.EdUserNameBD.Text;
-   Senha_BD   := RestDWForm.EdPasswordBD.Text;
-   TZConnection(Sender).Database := Database;
-   TZConnection(Sender).HostName := Servidor_BD;
-   TZConnection(Sender).Port     := StrToInt(Porta_BD);
-   TZConnection(Sender).User     := Usuario_BD;
-   TZConnection(Sender).Password := Senha_BD;
-   TZConnection(Sender).LoginPrompt := FALSE;
-End;
-
-procedure TServerMethodDM.IBConnection1BeforeConnect(Sender: TObject);
-VAR
-  Driver_BD: STRING;
-  Porta_BD: STRING;
-  Servidor_BD: STRING;
-  DataBase: STRING;
-  Pasta_BD: STRING;
-  Usuario_BD: STRING;
-  Senha_BD: STRING;
-BEGIN
-   database:= RestDWForm.EdBD.Text;
-   Driver_BD := RestDWForm.CbDriver.Text;
-   If RestDWForm.CkUsaURL.Checked Then
-    Servidor_BD := RestDWForm.EdURL.Text
-   Else
-    Servidor_BD := RestDWForm.DatabaseIP;
-   Case RestDWForm.CbDriver.ItemIndex Of
-    0 : Begin
-         Pasta_BD := IncludeTrailingPathDelimiter(RestDWForm.EdPasta.Text);
-         Database := RestDWForm.edBD.Text;
-         Database := Pasta_BD + Database;
-        End;
-    1 : Database := RestDWForm.EdBD.Text;
-   End;
-   Porta_BD   := RestDWForm.EdPortaBD.Text;
-   Usuario_BD := RestDWForm.EdUserNameBD.Text;
-   Senha_BD   := RestDWForm.EdPasswordBD.Text;
-   TIBConnection(Sender).DatabaseName := Database;
-   TIBConnection(Sender).HostName    := Servidor_BD;
-//   TIBConnection(Sender).Port        := StrToInt(Porta_BD);
-   TIBConnection(Sender).Username    := Usuario_BD;
-   TIBConnection(Sender).Password    := Senha_BD;
-   TIBConnection(Sender).LoginPrompt := FALSE;
-End;
-
-procedure TServerMethodDM.RESTDWServerEventsEventsservertimeReplyEvent(
-  var Params: TRESTDWParams; var Result: String);
+var
+  Driver_BD: string;
+  Porta_BD: string;
+  Servidor_BD: string;
+  DataBase: string;
+  Pasta_BD: string;
+  Usuario_BD: string;
+  Senha_BD: string;
 begin
- Params.ItemsString['result'].AsDateTime := Now;
+  database := fPrincipal.EdBD.Text;
+  Driver_BD := fPrincipal.CbDriver.Text;
+  if fPrincipal.CkUsaURL.Checked then
+    Servidor_BD := fPrincipal.EdURL.Text
+  else
+    Servidor_BD := fPrincipal.DatabaseIP;
+  case fPrincipal.CbDriver.ItemIndex of
+    0: begin
+      Pasta_BD := IncludeTrailingPathDelimiter(fPrincipal.EdPasta.Text);
+      Database := fPrincipal.edBD.Text;
+      Database := Pasta_BD + Database;
+    end;
+    1: Database := fPrincipal.EdBD.Text;
+  end;
+  Porta_BD := fPrincipal.EdPortaBD.Text;
+  Usuario_BD := fPrincipal.EdUserNameBD.Text;
+  Senha_BD := fPrincipal.EdPasswordBD.Text;
+  TZConnection(Sender).Database := Database;
+  TZConnection(Sender).HostName := Servidor_BD;
+  TZConnection(Sender).Port := StrToInt(Porta_BD);
+  TZConnection(Sender).User := Usuario_BD;
+  TZConnection(Sender).Password := Senha_BD;
+  TZConnection(Sender).LoginPrompt := False;
 end;
 
-procedure TServerMethodDM.DataModuleGetToken(Welcomemsg, AccessTag: String;
-  Params: TRESTDWParams; AuthOptions: TRESTDWAuthTokenParam;
-  var ErrorCode: Integer; var ErrorMessage: String; var TokenID: String;
-  var Accept: Boolean);
-Var
- vMyClient,
- vMyPass    : String;
+procedure TServerMethodDM.IBConnection1BeforeConnect(Sender: TObject);
+var
+  Driver_BD: string;
+  Porta_BD: string;
+  Servidor_BD: string;
+  DataBase: string;
+  Pasta_BD: string;
+  Usuario_BD: string;
+  Senha_BD: string;
 begin
- vMyClient  := '';
- vMyPass    := vMyClient;
- If (Params.ItemsString['username'] <> Nil) And
-    (Params.ItemsString['password'] <> Nil) Then
-  Begin
-   vMyClient  := Params.ItemsString['username'].AsString;
-   vMyPass    := Params.ItemsString['password'].AsString;
-  End
- Else
-  Begin
-   vMyClient  := Copy(Welcomemsg, InitStrPos, Pos('|', Welcomemsg) -1);
-   Delete(Welcomemsg, InitStrPos, Pos('|', Welcomemsg));
-   vMyPass    := Trim(Welcomemsg);
-  End;
- Accept     := Not ((vMyClient = '') Or
-                    (vMyPass   = ''));
- If Accept Then
-  Begin
-   ZQuery1.Close;
-   ZQuery1.SQL.Clear;
-   ZQuery1.SQL.Add('select * from TB_USUARIO where Upper(NM_LOGIN) = Upper(:NM_LOGIN) and Upper(DS_SENHA) = Upper(:DS_SENHA)');
-   Try
-    ZQuery1.ParamByName('NM_LOGIN').AsString := vMyClient;
-    ZQuery1.ParamByName('DS_SENHA').AsString := vMyPass;
-    ZQuery1.Open;
-   Finally
-    Accept     := Not(ZQuery1.EOF);
-    If Not Accept Then
-     Begin
-      ErrorMessage := cInvalidAuth;
-      ErrorCode  := 401;
-     End
-    Else
-     TokenID := AuthOptions.GetToken(Format('{"id":"%s", "login":"%s"}', [ZQuery1.FindField('ID_PESSOA').AsString,
-                                                                          ZQuery1.FindField('NM_LOGIN').AsString]));
+  database := fPrincipal.EdBD.Text;
+  Driver_BD := fPrincipal.CbDriver.Text;
+  if fPrincipal.CkUsaURL.Checked then
+    Servidor_BD := fPrincipal.EdURL.Text
+  else
+    Servidor_BD := fPrincipal.DatabaseIP;
+  case fPrincipal.CbDriver.ItemIndex of
+    0: begin
+      Pasta_BD := IncludeTrailingPathDelimiter(fPrincipal.EdPasta.Text);
+      Database := fPrincipal.edBD.Text;
+      Database := Pasta_BD + Database;
+    end;
+    1: Database := fPrincipal.EdBD.Text;
+  end;
+  Porta_BD := fPrincipal.EdPortaBD.Text;
+  Usuario_BD := fPrincipal.EdUserNameBD.Text;
+  Senha_BD := fPrincipal.EdPasswordBD.Text;
+  TIBConnection(Sender).DatabaseName := Database;
+  TIBConnection(Sender).HostName := Servidor_BD;
+  //   TIBConnection(Sender).Port        := StrToInt(Porta_BD);
+  TIBConnection(Sender).Username := Usuario_BD;
+  TIBConnection(Sender).Password := Senha_BD;
+  TIBConnection(Sender).LoginPrompt := False;
+end;
+
+procedure TServerMethodDM.RESTDWServerEventsEventsservertimeReplyEvent(
+  var Params: TRESTDWParams; var Result: string);
+begin
+  Params.ItemsString['result'].AsDateTime := Now;
+end;
+
+procedure TServerMethodDM.DataModuleGetToken(Welcomemsg, AccessTag: string;
+  Params: TRESTDWParams; AuthOptions: TRESTDWAuthTokenParam;
+  var ErrorCode: integer; var ErrorMessage: string; var TokenID: string;
+  var Accept: boolean);
+var
+  vMyClient, vMyPass: string;
+begin
+  vMyClient := '';
+  vMyPass := vMyClient;
+  if (Params.ItemsString['username'] <> nil) and
+    (Params.ItemsString['password'] <> nil) then
+  begin
+    vMyClient := Params.ItemsString['username'].AsString;
+    vMyPass := Params.ItemsString['password'].AsString;
+  end
+  else
+  begin
+    vMyClient := Copy(Welcomemsg, InitStrPos, Pos('|', Welcomemsg) - 1);
+    Delete(Welcomemsg, InitStrPos, Pos('|', Welcomemsg));
+    vMyPass := Trim(Welcomemsg);
+  end;
+  Accept := not ((vMyClient = '') or (vMyPass = ''));
+  if Accept then
+  begin
     ZQuery1.Close;
-   End;
-  End
- Else
-  Begin
-   ErrorMessage := cInvalidAuth;
-   ErrorCode  := 401;
-  End;
+    ZQuery1.SQL.Clear;
+    ZQuery1.SQL.Add(
+      'select * from TB_USUARIO where Upper(NM_LOGIN) = Upper(:NM_LOGIN) and Upper(DS_SENHA) = Upper(:DS_SENHA)');
+    try
+      ZQuery1.ParamByName('NM_LOGIN').AsString := vMyClient;
+      ZQuery1.ParamByName('DS_SENHA').AsString := vMyPass;
+      ZQuery1.Open;
+    finally
+      Accept := not (ZQuery1.EOF);
+      if not Accept then
+      begin
+        ErrorMessage := cInvalidAuth;
+        ErrorCode := 401;
+      end
+      else
+        TokenID := AuthOptions.GetToken(Format('{"id":"%s", "login":"%s"}',
+          [ZQuery1.FindField('ID_PESSOA').AsString,
+          ZQuery1.FindField('NM_LOGIN').AsString]));
+      ZQuery1.Close;
+    end;
+  end
+  else
+  begin
+    ErrorMessage := cInvalidAuth;
+    ErrorCode := 401;
+  end;
 end;
 
 
